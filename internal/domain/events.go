9818580d15fd55df38c2a5aba7ad48c9563cb45d
ma.maleki <ma.maleki@PC-1293>
2024-04-29 08:30:03 +0330

    Add allocation event

--- internal/domain/events.go
+++ internal/domain/events.go
@@ -6,7 +6,8 @@ import (
 )
 
 const (
-	OutOfStockEvent = "OutOfStock"
+	OutOfStockEvent         = "OutOfStock"
+	AllocationRequiredEvent = "OutOfStock"
 )
 
 type Event interface {
@@ -40,6 +41,41 @@ func (o OutOfStock) Name() string {
 	eventName := reflect.TypeOf(o).Name()
 	if eventName != OutOfStockEvent {
 		panic(fmt.Sprintf("event name collision, %v != %v", eventName, OutOfStockEvent))
+	}
+	return eventName
+}
+
+type AllocationRequired struct {
+	orderId, sku string
+	qty          int
+}
+
+func NewAllocationRequired(orderId string, sku string, qty int) *AllocationRequired {
+	return &AllocationRequired{orderId: orderId, sku: sku, qty: qty}
+}
+
+var _ Event = (*AllocationRequired)(nil)
+
+func (a AllocationRequired) Qty() int {
+	return a.qty
+}
+
+func (a AllocationRequired) Sku() string {
+	return a.sku
+}
+
+func (a AllocationRequired) OrderId() string {
+	return a.orderId
+}
+
+func (a AllocationRequired) ID() string {
+	return a.sku
+}
+
+func (a AllocationRequired) Name() string {
+	eventName := reflect.TypeOf(a).Name()
+	if eventName != AllocationRequiredEvent {
+		panic(fmt.Sprintf("event name collision, %v != %v", eventName, AllocationRequiredEvent))
 	}
 	return eventName
 }
